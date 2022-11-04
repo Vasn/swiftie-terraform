ed38dea6302fb985a13b7fcf5a509883fb828dd8
Vasn <vasilisngjunquan@gmail.com>
2022-11-05 02:22:22 +0800

    things for me to do....................

---
+++ todo.tf
@@ -0,0 +1,22 @@
+# 1. Create 1x VPC
+# 2. Create 2x subnet (1 public, 1 private)
+# 3. Create IGW
+# 4. Create custom route table
+# 5. Associate subnet with route table
+# 6. Create security group to allow web traffic & SSH (port 22, 80, 443)
+# 7. Create a network interface with ip in subnet (in step 2)
+# 8. Create webserver & apache
+# 9. Create Route 53
+# 10. Create WAF
+# 11. Create API gateway
+# 12. Create ELB
+# 13. Create ALB
+# 14. Create Router
+# 15. Create backend flask
+# 16. Create auto scaling group
+# 17. Create DynamoDB
+# 18. Create Datapipeline
+# 19. Create S3 for pipeline
+# 20. Create Lambda for GCP backup
+# 21. Create Storage Transfer Service (GCP)
+# 22. Create Cloud Storage (GCP)
